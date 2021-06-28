6f893030f96352f7aaa6b1fa7399333643c4dc02
AustinJohnAaro <austin1472a@gmail.com>
2021-06-28 13:22:40 -0700

    j

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var  express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var compression = require("compression");
-var path = require('path');
 
 var PORT = process.env.PORT || 3001;
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/taste";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-app";
 
 var app = express();
 
@@ -15,21 +14,17 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./models/public"));
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taste', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-app', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./models/routes/api.js"));
-
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, './models/public/index.html'));
-});
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-});
+}); 
